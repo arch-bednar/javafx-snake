44eb541eeffe0c839be1875bc00c645fa49dfb6f
Marvin <dawid.bednarczyk@protonmail.com>
2021-04-28 09:01:16 +0200

    snake and menu class

---
+++ Snake.java
@@ -0,0 +1,17 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class Snake extends Application{
+
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+
+
+    public static void main(String[] args){
+	
+    }
+}
